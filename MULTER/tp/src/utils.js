b68c456fec49abb0e7b819e9c277087bd2ad33de
Tomás Manoukian <tomanoukian@gmail.com>
2024-02-07 11:04:30 -0300

    Actualiza utils.js

--- MULTER/tp/src/utils.js
+++ MULTER/tp/src/utils.js
@@ -1,6 +1,6 @@
 import multer from 'multer';
 
-const currentDir = '/Users/Daniel/Documents/PROGRAMACION/CODECASA/clase8/tp'; 
+const currentDir = '/suRuta'; 
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
