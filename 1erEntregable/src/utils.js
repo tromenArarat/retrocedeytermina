97644f9c4e29d2b415e7ad431f866079587c733e
Tomás Manoukian <tomanoukian@gmail.com>
2024-02-07 11:01:22 -0300

    Actualiza utils.js

--- 1erEntregable/src/utils.js
+++ 1erEntregable/src/utils.js
@@ -1,6 +1,6 @@
 import multer from 'multer';
 
-const currentDir = '/Users/Daniel/Documents/PROGRAMACION/CODECASA/clase8/entregable'; 
+const currentDir = '/suRuta'; 
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
